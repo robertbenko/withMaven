e51bf4ddf451a27ff96f97ceba59d32221653208
Robert Benko <pl78174@emea.ibm.net>
2018-12-11 13:05:01 +0100

    first mv commit today

---
+++ Documents/Java/NaukaJavy/MavenProjects/mavenhelloworldapp/src/main/java/pl/edu/agh/mwo/mavenhelloworldapp/App.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.mwo.mavenhelloworldapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
